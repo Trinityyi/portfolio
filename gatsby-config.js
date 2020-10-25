a42156dd309d5abe40d622c936348529983e2436
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-10-25 12:08:08 +0200

    Init gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  siteMetadata: {
+    title: `Isabelle Viktoria Maciohsek - Portfolio`,
+    author: {
+      name: `Isabelle Viktoria Maciohsek`,
+      summary: `who lives and works in San Francisco building useful things.`,
+      location: `Athens, Greece`,
+      social: {
+        email: `mailto:imaciohsek@gmail.com`,
+        github: `https://github.com/Trinityyi`,
+        linkedin: `https://www.linkedin.com/in/isabelle-viktoria-maciohsek-aa6bb1129/`
+      }
+    },
+    description: `My personal portfolio and resume.`,
+    siteUrl: `https://maciv.netlify.app/`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          `gatsby-remark-copy-linked-files`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require('tailwindcss'),
+          require('./tailwind.config.js')
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+  ],
+};
