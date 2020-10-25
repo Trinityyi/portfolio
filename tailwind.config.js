2d133384be751d645db3aa34744ead3850c462a5
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-10-25 12:09:04 +0200

    Add static assets and styles

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      'sans': ['"Roboto"', 'Helvetica', 'Arial', 'sans-serif']
+    },
+  },
+  variants: {},
+  plugins: [],
+};
